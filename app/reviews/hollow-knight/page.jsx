84cd822dce8490819166fa73906b024edfa7e170
apm30 <m.ajaypradeep@icloud.com>
2023-10-05 18:45:27 +0100

    static assests are stored in the public folder added images and test.txt file.

--- app/reviews/hollow-knight/page.jsx
+++ app/reviews/hollow-knight/page.jsx
@@ -4,6 +4,7 @@ export default function HomePage() {
     return (
         <div>
             <Heading>Hollow Knight</Heading>
+            <img src="/images/hollow-knight.jpg" alt="Hollow Knight"  className="" style={{height:"200px", width :"200px"}}/>
             <p>
                 Hollow Knight is a 2D adventure/ Metroidvania game for PC, Mac, Linux, Nintendo Switch, PlayStation 4 and Xbox One!
             </p>
