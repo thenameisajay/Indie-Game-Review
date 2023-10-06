b7af4cf75fe1d1552bd700cd22575da80ba1040e
apm30 <m.ajaypradeep@icloud.com>
2023-10-06 11:10:57 +0100

    Reading Files from content/reviews.

--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {readFile} from 'node:fs/promises';
 import Heading from "@/components/Heading";
-export default function StardewValleyPage() {
+export default async function StardewValleyPage() {
+ const text = await readFile('./content/reviews/stardew-valley.md', 'utf-8');
+
     return (
         <div>
            <Heading>Stardew Valley</Heading>
-           <p>This will be the review for Stardew Valley.</p>
+           <img src ="/images/stardew-valley.jpg" alt='stardew-valley' width={320} height={180} className='mb-2 rounded'/>
+           <p>{text}</p>
         </div>
     )   ;
 };
