87cf798c5826a3cd72686a3a06af2ea27c306da5
apm30 <m.ajaypradeep@icloud.com>
2023-10-06 14:02:33 +0100

    made reviews dynamic routes.

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import Heading from "@/components/Heading";
 import {getReview} from "@/lib/reviews";
 
-export default async function StardewValleyPage() {
-
-const review = await getReview('stardew-valley');
+export default async function ReviewPage(props) {
+console.log(props);
+const review = await getReview(props.params.slug);
     return (
         <div>
            <Heading>{review.title}</Heading>
            <p className='italic pb-2'>{review.date}</p>
-           <img src ={review.image} alt='stardew-valley' width={640} height={360} className='mb-2 rounded'/>
+           <img src ={review.image} alt={props.params.slug} width={640} height={360} className='mb-2 rounded'/>
            <article dangerouslySetInnerHTML={{__html: review.body}} className='max-w-screen-sm prose prose-slate'/> 
 
         </div>
