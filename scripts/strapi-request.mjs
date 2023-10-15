1cdd85b59f8fbab5c3074f99e65c7f376b365367
apm30 <m.ajaypradeep@icloud.com>
2023-10-15 10:56:30 +0100

    modifying strapi-request api url

--- scripts/strapi-request.mjs
+++ scripts/strapi-request.mjs
@@ -1,16 +1,27 @@
-import {writeFileSync} from "node:fs";
+import { writeFileSync } from "node:fs";
+import qs from "qs";
 
-const URL =  process.env.NEXT_API_URL || "http://localhost:1337";
+const URL = process.env.NEXT_API_URL || "http://localhost:1337";
 
-const populate = "?populate=*"; // By default , strapi treats image as seperate object and does not include it in the api call , this code also includes the media in the call.
+const populate =
+  "?" +
+  qs.stringify(
+    {
+      fields: ["slug", "title", "subtitle", "publishedAt"],
+      populate: { image: { fields: ["url"] } },
+    },
+    { encodeValuesOnly: true }
+  ); // By default , strapi treats image as seperate object and does not include it in the api call , this code also includes the media in the call.
 
-const endpoint = URL + '/api/reviews' + populate ;
+const endpoint = URL + "/api/reviews" + populate;
 
-const response = await fetch (endpoint);
+console.log(endpoint);
+
+const response = await fetch(endpoint);
 
 const body = await response.json();
 
-const formatted = JSON.stringify(body,null,2);
+const formatted = JSON.stringify(body, null, 2);
 const file = "scripts/strapi-response.json";
 
-writeFileSync(file,formatted,'utf-8');
+writeFileSync(file, formatted, "utf-8");
